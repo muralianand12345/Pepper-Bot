03a583cca9472e13aa3f6c5bce0abfd437873f09
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2024-06-14 15:04:35 +0530

    💠3.3.1

--- src/events/guild/guildLog/exitGuild.ts
+++ src/events/guild/guildLog/exitGuild.ts
@@ -1,4 +1,4 @@
-import { Events } from 'discord.js';
+import { Events, EmbedBuilder } from 'discord.js';
 
 import { BotEvent } from '../../../types';
 import botAnalysisModal from '../../database/schema/botDataAnalysis';
@@ -16,6 +16,23 @@ const event: BotEvent = {
                 await botAnalysisData.save();
             }
         }
+
+        const embed = new EmbedBuilder()
+            .setTitle('Left Guild')
+            .setDescription(`I have been kicked from **${guild.name}** (${guild.id})`)
+            .setFields(
+                { name: 'Members', value: guild.memberCount.toString(), inline: true },
+                { name: 'Owner', value: guild.owner?.user.tag, inline: true },
+                { name: 'Region', value: guild.region, inline: true },
+                { name: 'Created', value: guild.createdAt.toDateString(), inline: true },
+            )
+            .setThumbnail(guild.iconURL() || '')
+            .setFooter({ text: `Guild ID: ${guild.id}` })
+            .setColor('Red')
+            .setTimestamp();
+
+        await client.channels.cache.get("1251103097632194620").send({ embeds: [embed] });
+
         client.logger.debug(`[GUILD] Kicked from guild ${guild.name} (${guild.id})`);
     }
 }
--- src/events/guild/guildLog/newGuild.ts
+++ src/events/guild/guildLog/newGuild.ts
@@ -1,4 +1,4 @@
-import { Events } from 'discord.js';
+import { Events, EmbedBuilder } from 'discord.js';
 
 import { BotEvent } from '../../../types';
 import botAnalysisModal from '../../database/schema/botDataAnalysis';
@@ -20,6 +20,24 @@ const event: BotEvent = {
             });
             await botAnalysisData.save();
         }
+
+        const embed = new EmbedBuilder()
+            .setTitle('New Guild')
+            .setDescription(`I have joined **${guild.name}** (${guild.id})`)
+            .setFields(
+                { name: 'Members', value: guild.memberCount.toString(), inline: true },
+                { name: 'Owner', value: guild.owner?.user.tag, inline: true },
+                { name: 'Region', value: guild.region, inline: true },
+                { name: 'Created', value: guild.createdAt.toDateString(), inline: true },
+            )
+            .setThumbnail(guild.iconURL() || '')
+            .setFooter({ text: `Guild ID: ${guild.id}` })
+            .setColor('Green')
+            .setTimestamp();
+
+
+        await client.channels.cache.get("1251103097632194620").send({ embeds: [embed] });
+
         client.logger.debug(`[GUILD] Joined guild ${guild.name} (${guild.id})`);
     }
 }
