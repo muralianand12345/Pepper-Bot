3b4d0c9d4bf37ba1caaf315d5450a87d733fdf7b
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2025-04-04 21:01:20 +0530

    Add setup command for music bot configuration and enhance message handling in song channel

--- src/events/music/music_events/music_channel.ts
+++ src/events/music/music_events/music_channel.ts
@@ -1,4 +1,5 @@
 import discord from "discord.js";
+import music_guild from "../../database/schema/music_guild";
 import { BotEvent } from "../../../types";
 
 const event: BotEvent = {
@@ -7,6 +8,26 @@ const event: BotEvent = {
         message: discord.Message,
         client: discord.Client
     ): Promise<void> => {
+
+        if (!message.guild) return;
+        if (message.author.bot) return;
+
+        const guild_data = await music_guild.findOne({
+            guildId: message.guild?.id,
+        });
+
+        if (!guild_data) return;
+        if (message.content.startsWith(guild_data.prefix)) return;
+
+        const song_channelId = guild_data.songChannelId;
+        if (message.channel.id !== song_channelId) return;
+
+        console.log(
+            `Message from ${message.author.username} in song channel: ${message.content}`
+        );
+
+
+
     }
 }
 
