290e1e83d88cd21c78f4de0807b14fb708589d09
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2025-06-20 14:18:06 +0530

    feat(lyrics): implement lyrics command with localization and execution logic

--- build/commands/lyrics.js
+++ build/commands/lyrics.js
@@ -1,5 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = __importDefault(require("discord.js"));
+const music_1 = require("../core/music");
 const locales_1 = require("../core/locales");
 const localizationManager = locales_1.LocalizationManager.getInstance();
-const localeDetector = new locales_1.LocaleDetector();
+const lyricsCommand = {
+    cooldown: 5,
+    data: new discord_js_1.default.SlashCommandBuilder()
+        .setName('lyrics')
+        .setDescription('Display lyrics for the currently playing track')
+        .setNameLocalizations(localizationManager.getCommandLocalizations('commands.lyrics.name'))
+        .setDescriptionLocalizations(localizationManager.getCommandLocalizations('commands.lyrics.description'))
+        .setContexts(discord_js_1.default.InteractionContextType.Guild)
+        .addBooleanOption((option) => option.setName('skip_track_source').setDescription("Skip using the track's source URL when searching for lyrics").setNameLocalizations(localizationManager.getCommandLocalizations('commands.lyrics.options.skip_track_source.name')).setDescriptionLocalizations(localizationManager.getCommandLocalizations('commands.lyrics.options.skip_track_source.description')).setRequired(false)),
+    execute: async (interaction, client) => {
+        const music = new music_1.Music(client, interaction);
+        await music.lyrics();
+    },
+};
+exports.default = lyricsCommand;
--- src/commands/lyrics.ts
+++ src/commands/lyrics.ts
@@ -2,7 +2,23 @@ import discord from 'discord.js';
 
 import { Command } from '../types';
 import { Music } from '../core/music';
-import { LocalizationManager, LocaleDetector } from '../core/locales';
+import { LocalizationManager } from '../core/locales';
 
 const localizationManager = LocalizationManager.getInstance();
-const localeDetector = new LocaleDetector();
+
+const lyricsCommand: Command = {
+	cooldown: 5,
+	data: new discord.SlashCommandBuilder()
+		.setName('lyrics')
+		.setDescription('Display lyrics for the currently playing track')
+		.setNameLocalizations(localizationManager.getCommandLocalizations('commands.lyrics.name'))
+		.setDescriptionLocalizations(localizationManager.getCommandLocalizations('commands.lyrics.description'))
+		.setContexts(discord.InteractionContextType.Guild)
+		.addBooleanOption((option) => option.setName('skip_track_source').setDescription("Skip using the track's source URL when searching for lyrics").setNameLocalizations(localizationManager.getCommandLocalizations('commands.lyrics.options.skip_track_source.name')).setDescriptionLocalizations(localizationManager.getCommandLocalizations('commands.lyrics.options.skip_track_source.description')).setRequired(false)),
+	execute: async (interaction: discord.ChatInputCommandInteraction, client: discord.Client): Promise<void> => {
+		const music = new Music(client, interaction);
+		await music.lyrics();
+	},
+};
+
+export default lyricsCommand;
