1cddec3f042516566faa9e8542e4eebae9b5b3de
Murali Anand <smurali1607@gmail.com>
2025-08-25 21:08:37 +0530

    feat(ai): migrate from Groq SDK to OpenAI for speech generation and update configuration management

--- src/core/ai/llm.ts
+++ src/core/ai/llm.ts
@@ -1,20 +1,36 @@
-import Groq from 'groq-sdk';
+import OpenAI from 'openai';
+
+import { ConfigManager } from '../../utils/config';
+
+const configManager = ConfigManager.getInstance();
 
 class LLM {
-	private client: Groq;
+	private client: OpenAI;
 
 	constructor() {
-		this.client = new Groq({ apiKey: process.env.GROQ_API_KEY });
+		this.client = new OpenAI({ apiKey: configManager.getOpenAiApiKey(), baseURL: configManager.getOpenAiBaseUrl() });
 	}
 
-    public generateSpeech = async (input: string, model: string = 'playai-tts', voice: string = 'Fritz-PlayAI', responseFormat: 'flac' | 'mp3' | 'mulaw' | 'ogg' | 'wav' = 'wav') => {
-        const response = await this.client.audio.speech.create({
-            model: model,
-            voice: voice,
-            input: input,
-            response_format: responseFormat
-        });
+	public invoke = async (input: string | OpenAI.Responses.ResponseInput | undefined, model: string = 'openai/gpt-oss-20b', options: any = {}): Promise<OpenAI.Responses.Response> => {
+		const response = this.client.responses.create({
+			input: input,
+			model: model,
+			...options,
+		});
 
-        const buffer = Buffer.from(await response.arrayBuffer());
-    }
+		return response;
+	};
+
+	public generateSpeech = async (input: string, model: string = 'playai-tts', voice: string = 'Fritz-PlayAI', responseFormat: 'wav' | 'mp3' | 'opus' | 'aac' | 'flac' | 'pcm' | undefined = 'wav') => {
+		const response = await this.client.audio.speech.create({
+			model: model,
+			voice: voice,
+			input: input,
+			response_format: responseFormat,
+		});
+
+		return Buffer.from(await response.arrayBuffer());
+	};
 }
+
+export default LLM;
